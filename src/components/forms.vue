7bd9d2a2a5fe7c9f3f01ce93667999650999931d
akacyman <akacyman@gmail.com>
2025-08-04 13:40:58 +1000

    STC

---
+++ src/components/forms.vue
@@ -0,0 +1,37 @@
+<template>
+
+<div>
+<br><br>
+
+As a great man once said, "Machette don't text."
+</div>
+
+</template>
+
+
+
+<script>
+  export default {
+
+    data: function() {
+       return {
+          message: '',
+          fullname: ''
+        }
+    },
+
+    methods: {
+      onSubmit() {
+        alert('submitting')
+      }
+      
+    }
+
+
+  }
+</script>
+
+<style>
+
+
+</style>
