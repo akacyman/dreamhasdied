5ad17fbf84255a3144238754c919fe021eb20118
akacyman <akacyman@gmail.com>
2025-08-04 11:11:07 +1000

    base name

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'akacyman.github.io/savethatcat',
+  base: 'akacyman.github.io/dreamhasdied',
   plugins: [
     vue(),
     vueDevTools(),
