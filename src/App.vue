2d04d9424cbcf2c042e22165d084990acdd1103b
akacyman <akacyman@gmail.com>
2025-08-05 10:04:24 +1000

    dream has died placeholder site

--- src/App.vue
+++ src/App.vue
@@ -7,28 +7,20 @@
     <menubox @menuselected="menuselect=3">Contact</menubox>
   </menubar>
 
-  <pagetemplate v-bind:menuselect=menuselect  v-bind:thisselect="2">
-          Creator<br><br>I'm just a guy<br>
+<pagetemplate :menuselect="menuselect" :thisselect="menuselect">
+   <template v-if="menuselect === 2">
+    Creator<br><br>I'm just a guy<br>
+  </template>
 
-  </pagetemplate>
+  <template v-else-if="menuselect === 3">
+    Please don't contact me.
+  </template>
 
-  <pagetemplate v-bind:menuselect=menuselect  v-bind:thisselect="3"> Contact <br><br><forms></forms></pagetemplate>
+  <template v-else >
+The dream has died.
+  </template>
 
-  <pagetemplate v-bind:menuselect=menuselect  v-bind:thisselect="1">
-    <comicpanel v-bind:src='panels[currentNum].src' v-bind:panel="panels[currentNum].panel"></comicpanel>   
-    <prevbttn @previtem="prevPanel()"></prevbttn>
-    <nextbttn @nextitem="nextPanel()"></nextbttn> 
-  </pagetemplate>
-
-  <pagetemplate v-bind:menuselect=menuselect  v-bind:thisselect="0"> 
-    
-          <div class="start-object-container noselect" @click="menuselect=1,currentNum=0">
-           <object class="landscape home-svg" data='@/assets/savethatcatmain_landscape.svg' type="image/svg+xml" width="100%" height="99%"></object>
-            <object class="portrait home-svg" data="@/assets/savethatcatmain_portrait.svg" type="image/svg+xml" width="100%" height="99%"></object>
-          </div>
-
-  </pagetemplate>
-
+</pagetemplate>
 
   <footer>&copy; Simon Mealings 2025</footer>
 
